11a69238e1db19c7f8428014ae28a986f53c5eb2
Kimat Boven <kimat@users.noreply.github.com>
2019-05-29 21:32:03 +0200

    I'll try to solve this with the macro section

--- keyboards/handwired_preonic/keymaps/default/keymap.c
+++ keyboards/handwired_preonic/keymaps/default/keymap.c
@@ -18,17 +18,15 @@
 #include <sendstring_belgian.h>
 
 #define _BASE 0
-#define _ALTGR 1
-#define _SHIFT 2
-#define _SPACE 3
-#define _SHFSP 4
+#define _SPACE 1
+#define _ALTGR 2
 
-#define THUMB_G LT(_SHIFT,KC_ESC)
 #define THUMB_F LT(_SPACE,KC_SPC)
 #define THUMB_K LT(_ALTGR,KC_TAB)
-#define _LSFT LT(_SHIFT,KC_F12)
 
 #define THUMB_H LSFT_T(KC_BSPC)
+#define THUMB_G LSFT_T(KC_ESC)
+#define _LSFT LSFT_T(KC_F12)
 #define THUMB_J RCTL_T(KC_ENT)
 #define _CAPS LCTL_T(KC_ESC)
 
@@ -45,6 +43,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _CAPS         , KC_A    , KC_S    , KC_D    , KC_F      , KC_G    , KC_H       , KC_J       , KC_K     , KC_L        , KC_SCLN    , KC_ENT     , \
         _LSFT         , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , KC_N       , BE_COMM    , BE_DOT   , KC_DOT      , KC_SLSH    , KC_RSFT    , \
         KC_LCTL       , KC_LGUI , KC_LGUI , KC_LALT , THUMB_F   , THUMB_G , THUMB_H    , THUMB_J    , THUMB_K  , KC_K        , KC_L       , KC_CAPS  \
+       )             ,
+  // space
+[_SPACE] = LAYOUT ( \
+        _______       , _______ , _______ , _______ , _______   , _______ , _______    , _______    , BE_LCBR  , BE_RCBR     , _______    , _______    ,
+        KC_TAB        , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_HOME    , BE_LCBR  , BE_RCBR     , KC_DEL     , KC_PGUP    , \
+        _CAPS         , BE_PIPE , BE_MINS , KC_TAB  , BE_SLSH   , BE_PND  , KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , KC_END     , KC_PGDN    , \
+        _______       , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , KC_BSPC    , KC_M       , KC_COMM  , KC_DOT      , KC_SLSH    , KC_RSFT    , \
+        KC_LCTL       , KC_LGUI , _______ , KC_LALT , _______   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
         )             ,
 // altgr
 [_ALTGR] = LAYOUT ( \
@@ -52,32 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB        , BE_PIPE , BE_AT   , BE_HASH , KC_R      , KC_T    , KC_Y       , S(KC_7)    , S(KC_8)  , S(KC_9)     , KC_P       , KC_PGUP    , \
         _CAPS         , BE_PIPE , BE_UNDS , KC_D    , KC_SLSH   , KC_G    , KC_BSPC    , S(KC_4)    , S(KC_5)  , S(KC_6)     , KC_SCLN    , BE_GRV     , \
         _LSFT         , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , S(KC_0)    , S(KC_1)    , S(KC_2)  , S(KC_3)     , QMKBEST    , KC_RSFT    , \
-        KC_LCTL       , KC_LGUI , _______ , KC_LALT , THUMB_F   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-        )             ,
-// shift
-[_SHIFT] = LAYOUT ( \
-        S(KC_ESC)     , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4)   , S(KC_5) , S(KC_6)    , S(KC_7)    , S(KC_8)  , S(KC_9)     , S(KC_0)    , KC_A       , \
-        S(KC_TAB)     , S(KC_Q) , S(KC_W) , S(KC_E) , S(KC_R)   , S(KC_T) , S(KC_Y)    , S(KC_U)    , S(KC_I)  , S(KC_O)     , S(KC_P)    , S(KC_LBRC) , \
-        _CAPS         , S(KC_A) , S(KC_S) , S(KC_D) , S(KC_F)   , S(KC_G) , S(KC_H)    , S(KC_J)    , S(KC_K)  , S(KC_L)     , S(KC_SCLN) , S(KC_ENT)  , \
-        _______       , S(KC_Z) , S(KC_X) , S(KC_C) , S(KC_V)   , S(KC_B) , S(KC_N)    , S(BE_COMM) , BE_SCLN  , S(KC_DOT)   , S(KC_SLSH) , S(KC_RSFT) , \
-        KC_LCTL       , KC_LGUI , _______ , KC_LALT , S(KC_SPC) , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-        )             ,
- // space
-[_SPACE] = LAYOUT ( \
-        _______       , _______ , _______ , _______ , _______   , _______ , _______    , _______    , BE_LCBR  , BE_RCBR     , _______    , _______    ,
-        KC_TAB        , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_HOME    , BE_LCBR  , BE_RCBR     , KC_DEL     , KC_PGUP    , \
-        _CAPS         , BE_PIPE , BE_MINS , KC_TAB  , BE_SLSH   , BE_PND  , KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , KC_END     , KC_PGDN    , \
-        _______       , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , KC_BSPC    , KC_M       , KC_COMM  , KC_DOT      , KC_SLSH    , KC_RSFT    , \
-        KC_LCTL       , KC_LGUI , _______ , KC_LALT , THUMB_F   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-        )             ,
-// shifted  space
-[_SHFSP] = LAYOUT ( \
-        S(KC_ESC)     , S(KC_1) , S(KC_2) , S(KC_3) , S(KC_4)   , S(KC_5) , S(KC_6)    , S(KC_7)    , S(KC_8)  , S(KC_9)     , S(KC_0)    , KC_A       , \
-        S(KC_TAB)     , S(KC_Q) , S(KC_W) , S(KC_E) , S(KC_R)   , S(KC_T) , S(KC_Y)    , S(KC_U)    , S(KC_I)  , S(KC_O)     , S(KC_P)    , S(KC_LBRC) , \
-        _CAPS         , BE_PIPE , BE_MINS , KC_TAB  , BE_SLSH   , KC_G    , S(KC_LEFT) , S(KC_DOWN) , S(KC_UP) , S(KC_RIGHT) , S(KC_END)  , S(KC_PGDN) , \
-        _______       , S(KC_Z) , S(KC_X) , S(KC_C) , S(KC_V)   , S(KC_B) , S(KC_N)    , S(BE_COMM) , BE_SCLN  , S(KC_DOT)   , S(KC_SLSH) , S(KC_RSFT) , \
         KC_LCTL       , KC_LGUI , _______ , KC_LALT , _______   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-       )              ,
+        )             ,
   /* [0] = tkl */
   /*   KC_ESC         , KC_F1   , KC_F2   , KC_F3   , KC_F4     , KC_F5   , KC_F6      , KC_F7      , KC_F8    , KC_F9       , KC_F10     , KC_F11     , KC_F12  , KC_PSCR , KC_SLCK , KC_PAUS , */
   /*   KC_GRV         , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_7       , KC_8     , KC_9        , KC_0       , KC_MINS    , KC_EQL  , KC_BSPC , KC_INS  , KC_HOME , KC_PGUP , */
