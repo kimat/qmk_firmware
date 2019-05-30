251921f683d75353acc9b0c8b435bf9a718b5fbf
Kimat Boven <kimat@users.noreply.github.com>
2019-05-30 09:43:17 +0200

    wip

--- keyboards/handwired_preonic/config.h
+++ keyboards/handwired_preonic/config.h
@@ -249,7 +249,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 // #define BOOTMAGIC_LITE_ROW 0
 // #define BOOTMAGIC_LITE_COLUMN 0
-#define TAPPING_TERM 120
-#define RETRO_TAPPING
-/* #define IGNORE_MOD_TAP_INTERRUPT */
-/* #define PERMISSIVE_HOLD */
+/* #define TAPPING_TERM 120 */
+/* #define RETRO_TAPPING */
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/handwired_preonic/keymaps/default/keymap.c
+++ keyboards/handwired_preonic/keymaps/default/keymap.c
@@ -17,12 +17,12 @@
 #include "keymap_belgian.h"
 #include <sendstring_belgian.h>
 
-#define _BASE 0
-#define _SPACE 1
-#define _ALTGR 2
+#define _BA 0
+#define _SP 1
+#define _AG 2
 
-#define THUMB_F LT(_SPACE,KC_SPC)
-#define THUMB_K LT(_ALTGR,KC_TAB)
+#define THUMB_F LT(_SP,KC_SPC)
+#define THUMB_K LT(_AG,KC_TAB)
 
 #define THUMB_H LSFT_T(KC_BSPC)
 #define THUMB_G LSFT_T(KC_ESC)
@@ -37,36 +37,36 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_BASE] = LAYOUT ( \
-        KC_ESC        , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_7       , KC_8     , KC_9        , KC_0       , BE_MINS    , \
-        KC_TAB        , KC_Q    , KC_W    , KC_E    , KC_R      , KC_T    , KC_Y       , KC_U       , KC_I     , KC_O        , KC_P       , KC_LBRC    , \
-        _CAPS         , KC_A    , KC_S    , KC_D    , KC_F      , KC_G    , KC_H       , KC_J       , KC_K     , KC_L        , KC_SCLN    , KC_ENT     , \
-        _LSFT         , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , KC_N       , BE_COMM    , BE_DOT   , KC_DOT      , KC_SLSH    , KC_RSFT    , \
-        KC_LCTL       , KC_LGUI , KC_LGUI , KC_LALT , THUMB_F   , THUMB_G , THUMB_H    , THUMB_J    , THUMB_K  , KC_K        , KC_L       , KC_CAPS  \
-       )             ,
+[_BA] = LAYOUT ( \
+        KC_ESC     , BE_AMP  , BE_EXLM , BE_QUOT , BE_APOS , BE_LPRN , BE_RPRN , BE_LSBR , BE_RSBR , BE_RCBR  , KC_0    , BE_MINS    , \
+        KC_TAB     , BE_A    , BE_Z    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O     , KC_P    , KC_LBRC    , \
+        _CAPS      , BE_Q    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L     , BE_M    , KC_ENT     , \
+        _LSFT      , BE_W    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , BE_COMM , BE_DOT  , KC_DOT   , KC_SLSH , KC_RSFT    , \
+        KC_LCTL    , KC_LGUI , KC_LGUI , KC_LALT , THUMB_F , THUMB_G , THUMB_H , THUMB_J , THUMB_K , KC_K     , KC_L    , KC_CAPS  \
+       )           ,
   // space
-[_SPACE] = LAYOUT ( \
-        _______       , _______ , _______ , _______ , _______   , _______ , _______    , _______    , BE_LCBR  , BE_RCBR     , _______    , _______    ,
-        KC_TAB        , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_HOME    , BE_LCBR  , BE_RCBR     , KC_DEL     , KC_PGUP    , \
-        _CAPS         , BE_PIPE , BE_MINS , KC_TAB  , BE_SLSH   , BE_PND  , KC_LEFT    , KC_DOWN    , KC_UP    , KC_RIGHT    , KC_END     , KC_PGDN    , \
-        _______       , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , KC_BSPC    , KC_M       , KC_COMM  , KC_DOT      , KC_SLSH    , KC_RSFT    , \
-        KC_LCTL       , KC_LGUI , _______ , KC_LALT , _______   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-        )             ,
+[_SP] = LAYOUT ( \
+        _______    , _______ , _______ , _______ , _______ , _______ , _______ , _______ , BE_LCBR , BE_RCBR  , _______ , _______    ,
+        KC_TAB     , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_HOME , BE_LCBR , BE_RCBR  , KC_DEL  , KC_PGUP    , \
+        _CAPS      , BE_PIPE , BE_MINS , KC_TAB  , BE_SLSH , BE_PND  , KC_LEFT , KC_DOWN , KC_UP   , KC_RIGHT , KC_END  , KC_PGDN    , \
+        _______    , BE_LESS , BE_GRTR , KC_C    , KC_V    , KC_B    , KC_BSPC , KC_M    , KC_COMM , KC_DOT   , KC_SLSH , KC_RSFT    , \
+        KC_LCTL    , KC_LGUI , _______ , KC_LALT , _______ , _______ , THUMB_H , THUMB_J , _______ , KC_K     , KC_L    , KC_CAPS  \
+        )          ,
 // altgr
-[_ALTGR] = LAYOUT ( \
-        KC_ESC        , BE_PIPE , BE_AT   , BE_HASH , KC_4      , KC_5    , KC_6       , S(KC_7)    , S(KC_8)  , S(KC_9)     , KC_KP_0    , KC_A       , \
-        KC_TAB        , BE_PIPE , BE_AT   , BE_HASH , KC_R      , KC_T    , KC_Y       , S(KC_7)    , S(KC_8)  , S(KC_9)     , KC_P       , KC_PGUP    , \
-        _CAPS         , BE_PIPE , BE_UNDS , KC_D    , KC_SLSH   , KC_G    , KC_BSPC    , S(KC_4)    , S(KC_5)  , S(KC_6)     , KC_SCLN    , BE_GRV     , \
-        _LSFT         , KC_Z    , KC_X    , KC_C    , KC_V      , KC_B    , S(KC_0)    , S(KC_1)    , S(KC_2)  , S(KC_3)     , QMKBEST    , KC_RSFT    , \
-        KC_LCTL       , KC_LGUI , _______ , KC_LALT , _______   , _______ , THUMB_H    , THUMB_J    , _______  , KC_K        , KC_L       , KC_CAPS  \
-        )             ,
+[_AG] = LAYOUT ( \
+        KC_ESC     , BE_PIPE , BE_AT   , BE_HASH , KC_4    , KC_5    , KC_6    , S(KC_7) , S(KC_8) , S(KC_9)  , KC_KP_0 , KC_A       , \
+        KC_TAB     , BE_PIPE , BE_AT   , BE_HASH , KC_R    , KC_T    , KC_Y    , S(KC_7) , S(KC_8) , S(KC_9)  , KC_P    , KC_PGUP    , \
+        _CAPS      , BE_PIPE , BE_UNDS , KC_D    , KC_SLSH , KC_G    , KC_BSPC , S(KC_4) , S(KC_5) , S(KC_6)  , KC_SCLN , BE_GRV     , \
+        _LSFT      , BE_BSLS , KC_X    , KC_C    , KC_V    , KC_B    , S(KC_0) , S(KC_1) , S(KC_2) , S(KC_3)  , QMKBEST , KC_RSFT    , \
+        KC_LCTL    , KC_LGUI , _______ , KC_LALT , _______ , _______ , THUMB_H , THUMB_J , _______ , KC_K     , KC_L    , KC_CAPS  \
+        )          ,
   /* [0] = tkl */
-  /*   KC_ESC         , KC_F1   , KC_F2   , KC_F3   , KC_F4     , KC_F5   , KC_F6      , KC_F7      , KC_F8    , KC_F9       , KC_F10     , KC_F11     , KC_F12  , KC_PSCR , KC_SLCK , KC_PAUS , */
-  /*   KC_GRV         , KC_1    , KC_2    , KC_3    , KC_4      , KC_5    , KC_6       , KC_7       , KC_8     , KC_9        , KC_0       , KC_MINS    , KC_EQL  , KC_BSPC , KC_INS  , KC_HOME , KC_PGUP , */
-  /*   KC_TAB         , KC_Q    , KC_W    , KC_E    , KC_R      , KC_T    , KC_Y       , KC_U       , KC_I     , KC_O        , KC_P       , KC_LBRC    , KC_RBRC , KC_DEL  , KC_END  , KC_PGDN , */
-  /*   KC_CAPS        , KC_A    , KC_S    , KC_D    , KC_F      , KC_G    , KC_H       , KC_J       , KC_K     , KC_L        , KC_SCLN    , KC_QUOT    , KC_NUHS , KC_ENT  , */
-  /*   KC_LSFT        , KC_NUBS , KC_Z    , KC_X    , KC_C      , KC_V    , KC_B       , KC_N       , KC_M     , KC_COMM     , KC_DOT     , KC_SLSH    , KC_RSFT , KC_UP   , */
-  /*   KC_LCTL        , KC_LGUI , KC_LALT , KC_SPC  , KC_RALT   , KC_RGUI , KC_APP     , KC_RCTL    , KC_LEFT  , KC_DOWN     , KC_RGHT */
+  /*   KC_ESC      , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9    , KC_F10  , KC_F11     , KC_F12  , KC_PSCR , KC_SLCK , KC_PAUS , */
+  /*   KC_GRV      , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9     , KC_0    , KC_MINS    , KC_EQL  , KC_BSPC , KC_INS  , KC_HOME , KC_PGUP , */
+  /*   KC_TAB      , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O     , KC_P    , KC_LBRC    , KC_RBRC , KC_DEL  , KC_END  , KC_PGDN , */
+  /*   KC_CAPS     , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L     , KC_SCLN , KC_QUOT    , KC_NUHS , KC_ENT  , */
+  /*   KC_LSFT     , KC_NUBS , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_N    , KC_M    , KC_COMM  , KC_DOT  , KC_SLSH    , KC_RSFT , KC_UP   , */
+  /*   KC_LCTL     , KC_LGUI , KC_LALT , KC_SPC  , KC_RALT , KC_RGUI , KC_APP  , KC_RCTL , KC_LEFT , KC_DOWN  , KC_RGHT , */
   /* ) \ */
 
 };
