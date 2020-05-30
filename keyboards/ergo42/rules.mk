e6e21b614d8fc7cb1b6958e722fe9a6259c42900
Kimat Boven <kimat@users.noreply.github.com>
2020-05-30 12:48:07 +0200

    wip

--- keyboards/ergo42/rules.mk
+++ keyboards/ergo42/rules.mk
@@ -36,3 +36,14 @@ SPLIT_KEYBOARD = yes
 LAYOUTS = ortho_4x14
 
 DEFAULT_FOLDER = ergo42/rev1
+
+
+NKRO_ENABLE = no            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = no           # Audio output on port C6
+UNICODE_ENABLE = no         # Unicode
+BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+SUBPROJECT_rev1 = yes
+USE_I2C = yes
